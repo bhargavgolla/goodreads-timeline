2c173abcaae7cec103f2eef9ad06f2f7c1ba5463
Bhargav Golla <bgolla@linkedin.com>
2017-03-24 15:11:16 -0700

    First commit after boilerplate. Had to install passport-goodreads and goodreads using npm install --save-dev

---
+++ controllers/home.js
@@ -0,0 +1,38 @@
+/**
+ * GET /
+ * Home page.
+ */
+const goodreads = require('goodreads');
+
+const goodreads_client = new goodreads.client({
+  'key': process.env.GOODREADS_KEY,
+  'secret': process.env.GOODREADS_SECRET
+});
+
+exports.index = (req, res) => {
+  var review = new Array();
+  if (req.user && req.user.goodreads) {
+    goodreads_client.getSingleShelf({
+      userID: req.user.goodreads,
+      shelf: 'read',
+      sort: 'date_read',
+      order: 'd',
+      page: 1,
+      per_page: 20,
+      v: 2
+    }, function(json) {
+      if (json) {
+        reviews = json.GoodreadsResponse.reviews[0].review;
+        res.render('home', {
+          title: 'Home',
+          reviews: reviews
+        });
+      }
+    });
+  } else {
+    res.render('home', {
+      title: 'Home',
+      reviews: reviews
+    });
+  }
+};
